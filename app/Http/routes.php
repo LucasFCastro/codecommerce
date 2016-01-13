cf8b7ba394914c2008ae67af288a65e819a6e670
Lucas Castro <lucasfcastro@yahoo.com.br>
2016-01-12 22:50:55 -0300

    Commit 03.1 - Exercicio: Criação das rotas

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -20,7 +20,7 @@ Route::group(['prefix'=>'admin', 'as' => 'admin.'], function(){
     Route::group(['prefix'=>'categories', 'as'=>'categories.'], function(){
         Route::get('',          ['as'=>'index', 'uses'=> 'AdminCategoriesController@index']);
         Route::post('',         ['as'=>'store', 'uses'=> 'AdminCategoriesController@store']);
-        Route::get('/{id}',     ['as'=>'update', 'uses'=> 'AdminCategoriesController@show']);
+        Route::get('/{id}',     ['as'=>'show', 'uses'=> 'AdminCategoriesController@show']);
         Route::put('/{id}',     ['as'=>'update', 'uses'=> 'AdminCategoriesController@update']);
         Route::delete('/{id}',  ['as'=>'delete', 'uses'=> 'AdminCategoriesController@delete']);
     });
@@ -28,7 +28,7 @@ Route::group(['prefix'=>'admin', 'as' => 'admin.'], function(){
     Route::group(['prefix'=>'products', 'as'=>'products.'], function(){
         Route::get('',          ['as'=>'index', 'uses'=> 'AdminProductsController@index']);
         Route::post('',         ['as'=>'store', 'uses'=> 'AdminProductsController@store']);
-        Route::get('/{id}',     ['as'=>'update', 'uses'=> 'AdminProductsController@show']);
+        Route::get('/{id}',     ['as'=>'show', 'uses'=> 'AdminProductsController@show']);
         Route::put('/{id}',     ['as'=>'update', 'uses'=> 'AdminProductsController@update']);
         Route::delete('/{id}',  ['as'=>'delete', 'uses'=> 'AdminProductsController@delete']);
     });
