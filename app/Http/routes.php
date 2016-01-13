b6e46a4865a36c45515b689dd0b60628c5a94176
Lucas Castro <lucasfcastro@yahoo.com.br>
2016-01-12 22:46:08 -0300

    Commit 03.1 - Exercicio: Criação das rotas

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,19 +18,19 @@ Route::get('/', function () {
 Route::group(['prefix'=>'admin', 'as' => 'admin'], function(){
 
     Route::group(['prefix'=>'categories', 'as'=>'categories'], function(){
-        Route::get('',          'AdminCategoriesController@index');
-        Route::post('',         'AdminCategoriesController@store');
-        Route::get('/{id}',     'AdminCategoriesController@show');
-        Route::put('/{id}',     'AdminCategoriesController@update');
-        Route::delete('/{id}',  'AdminCategoriesController@destroy');
+        Route::get('',          ['as'=>'index', 'uses'=> 'AdminCategoriesController@index']);
+        Route::post('',         ['as'=>'store', 'uses'=> 'AdminCategoriesController@store']);
+        Route::get('/{id}',     ['as'=>'update', 'uses'=> 'AdminCategoriesController@show']);
+        Route::put('/{id}',     ['as'=>'update', 'uses'=> 'AdminCategoriesController@update']);
+        Route::delete('/{id}',  ['as'=>'delete', 'uses'=> 'AdminCategoriesController@delete']);
     });
 
     Route::group(['prefix'=>'products', 'as'=>'products'], function(){
-        Route::get('',          'AdminProductsController@index');
-        Route::post('',         'AdminProductsController@store');
-        Route::get('/{id}',     'AdminProductsController@show');
-        Route::put('/{id}',     'AdminProductsController@update');
-        Route::delete('/{id}',  'AdminProductsController@destroy');
+        Route::get('',          ['as'=>'index', 'uses'=> 'AdminProductsController@index']);
+        Route::post('',         ['as'=>'store', 'uses'=> 'AdminProductsController@store']);
+        Route::get('/{id}',     ['as'=>'update', 'uses'=> 'AdminProductsController@show']);
+        Route::put('/{id}',     ['as'=>'update', 'uses'=> 'AdminProductsController@update']);
+        Route::delete('/{id}',  ['as'=>'delete', 'uses'=> 'AdminProductsController@delete']);
     });
 
 });
